289d31c8bac014705f98dfa643b5b91fd5213094
Mark Moeckel <heymark@users.noreply.github.com>
2015-07-06 20:32:19 -0400

    Initial commit of triangle.js

---
+++ javascript/triangle/triangle.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var Triangle = function () {
+
+    var sides = [].slice.apply(arguments).map(function (value) {
+        return value > 0 ? value : 0;
+    }).sort(function (a, b) {
+        return a > b;
+    });
+
+    return {
+        kind: function () {
+            if (sides.indexOf(0) !== -1 || sides.length !== 3 || sides[2] > sides[0] + sides[1]) {
+                throw new Error("Invalid input.");
+            }
+            if (sides[0] === sides[2]) { return "equilateral"; } 
+            else if (sides[1] === sides[2]) { return "isosceles"; } 
+            else { return "scalene"; }
+        }
+    };
+};
+
+module.exports = Triangle;
