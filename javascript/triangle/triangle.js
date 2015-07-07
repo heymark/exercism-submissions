79cc961a72584eab2bc10cb2d60799d2c9518164
Mark Moeckel <heymark@users.noreply.github.com>
2015-07-07 07:54:41 -0400

    Simplify check for zero-length edges

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -10,7 +10,7 @@ var Triangle = function () {
 
     return {
         kind: function () {
-            if (sides.indexOf(0) !== -1 || sides.length !== 3 || sides[2] > sides[0] + sides[1]) {
+            if (sides[0] === 0 || sides.length !== 3 || sides[2] > sides[0] + sides[1]) {
                 throw new Error("Invalid input.");
             }
             if (sides[0] === sides[2]) { return "equilateral"; } 
