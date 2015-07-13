4432c34516ec2a9405d43bbc7ecfd38ab8a7380a
Mark Moeckel <heymark@users.noreply.github.com>
2015-07-13 00:08:39 -0400

    Replace custom Array.sort() with Array.reverse()

--- javascript/roman-numerals/roman-numerals.js
+++ javascript/roman-numerals/roman-numerals.js
@@ -6,7 +6,7 @@ var ConvertToRomanNumeral = function (number) {
         MAP = { 1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M" };
         
     // Since property keys are returned in numerical order as of ES2015, create a sorted array to loop through MAP in reverse
-    var keyArray = Object.keys(MAP).sort(function (a, b) { return parseInt(b) - parseInt(a); });
+    var keyArray = Object.keys(MAP).reverse();
     
     // Build the Roman numeral string recursively by iterating over each place value, from largest to smallest
     function buildromanNumeralString(remainder, keyArrayIndex) {
